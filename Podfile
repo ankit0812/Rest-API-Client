bcb42d673d7b13cc656d63ef61bec12ebe51a80a
ankit0812 <ankitkumargupta0812@gmail.com>
2016-09-02 12:11:06 +0530

    adding necessary files from pod update

--- Podfile
+++ Podfile
@@ -1,6 +1,9 @@
 target 'Rest API Client' do
 pod ’SWRevealViewController’
 pod ’AFNetworking’
+use_frameworks!
+pod 'Fabric'
+pod 'Crashlytics'
 end
 
 target 'Rest API ClientTests' do
--- Pods/Target Support Files/Pods-Rest API Client/Pods-Rest API Client-frameworks.sh
+++ Pods/Target Support Files/Pods-Rest API Client/Pods-Rest API Client-frameworks.sh
@@ -82,3 +82,12 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-Rest API Client/AFNetworking.framework"
+  install_framework "Pods-Rest API Client/SWRevealViewController.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-Rest API Client/AFNetworking.framework"
+  install_framework "Pods-Rest API Client/SWRevealViewController.framework"
+fi
